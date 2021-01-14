e4f1e6046d2dc5d9fb8cfb0e1f62c5fc0c8487af
Charl-Roger <76068545+Charl-Roger@users.noreply.github.com>
2021-01-14 22:07:05 +0000

    Add files via upload
    More changes made -  mainly putting in correct variables into graphing functions and correcting titles/labels

--- netcdf_plotting.py
+++ netcdf_plotting.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 12 18:36:11 2021
-
-@author: charlotte
-"""
-
 #################    PX913 MINI PROJECT      #################
 
 #This part was written by Charlotte Rogerson
@@ -18,12 +10,15 @@ Created on Tue Jan 12 18:36:11 2021
 
 #Uploading the NetCDF file
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import netCDF4 as nc
 import numpy as np
 
+
 #Accessing the dataset
 file = nc.Dataset('filename.nc4', mode='r', format="NETCDF4") 
-#reads in the file
+#Sanity checks - uncomment if not needed
+print(file)
 
 #Defining the variables in the file for the pseudocolour plot
 #Only need Ex, Ey - Electric fields
@@ -40,19 +35,20 @@ pos = file.variables['Position']
 
 #Sanity checks - uncomment if not needed
 print(pos)
+#pos[:,0] = x_data, pos[:,1] = y_data, 
 
 #This sets the grid size - possibly change later to be user defined
 xdata = np.linspace(-1, 1, 100)
 ydata = np.linspace(-1, 1, 100)
 
-#Sanity checks - uncomment if not needed
-print(file)
-
 
 #Pseudocolour plot of the variable Ex
 #If it is being too slow use instead pcolormesh
 #Need a 2d grid
-plt.figure(1)
+fig, axs = plt.subplots(1)
+
+z = ax.pcolor(xdata, ydata, Ex, cmap = 'RdGy' )
+
 plt.title('Electric Field (Ex)')
 plt.xlabel('X')
 plt.ylabel('Y')
@@ -62,9 +58,10 @@ plt.show()
 
 #Scatter graph of particle posion in x vs y
 plt.figure(2)
-plt.scatter(x,y)
 
-plt.xlabel('Particle position in X')
-plt.ylabel('Particle position in Y')
+plt.scatter(pos[:,0], pos[:,1])
+plt.title('Electron Position')
+plt.xlabel('Electron position in X')
+plt.ylabel('Electron position in Y')
 
 plt.show()
